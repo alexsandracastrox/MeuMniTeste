c6d614331aa22234049af9b7a25cb1393f2cef05
Alexsandra Castro <121517625+alexsandracastrox@users.noreply.github.com>
2023-03-24 15:29:13 -0300

    Meu Teste 1
    Nunca é tarde para estudar.

---
+++ src/test/java/Teste/mytest.java
@@ -0,0 +1,15 @@
+package Teste;
+
+import org.junit.jupiter.api.Test;
+
+public class mytest {
+    @Test
+    public void testFazerLoginNoTaskit() {
+
+        isTrue(1, 1 );
+    }
+    private void isTrue(int i, int i1) {
+    }
+
+
+}
